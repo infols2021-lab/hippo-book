553c819ac47bc7791e73c9cc9781fe91fcf89401
admin <info.ls.2021@gmail.com>
2026-09-10 13:03:50 +0300

    feat: smart configuration loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
     description:
       "Интерактивная подготовка к международной олимпиаде Hippo и экзаменам Gatehouse Awards: тесты, аудирование, кроссворды и аналитика прогресса.",
     locale: "ru_RU",
-    images: ["/skills.png"], // <-- Обложка подключена здесь
+    images: ["https://hipposha-book.ru/skills.png"], // <-- Обложка подключена здесь
   },
 };
 
